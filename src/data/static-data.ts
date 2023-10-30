38059cb1a5dc5cd9aff7f5cfa666df8bd045b2f0
Dewyn Liriano <dewyn.liriano@gmail.com>
2023-10-30 12:33:48 -0400

    updated work experiences

--- src/data/static-data.ts
+++ src/data/static-data.ts
@@ -1,13 +1,22 @@
 export const workExperience = [
 	{
 		title: 'Software Engineer',
+		company: 'FranklinCovey',
+		employmentTime: '04/23 - Present',
+		tasks: [
+			'Develop a Client Dashboard with React and Django to show dashboards and metrics',
+			'Develop a React based workflows'
+		]
+	},
+	{
+		title: 'Software Engineer',
 		company: 'EPAM Systems',
 		employmentTime: '05/22 - 12/22',
 		tasks: [
 			'Develop a highly accessible React component library to use in a couple of other projects for a client.',
 			'Create unit tests for the React components developed.',
-			'Create set of requirements for the front end components APIs.',
-		],
+			'Create set of requirements for the front end components APIs.'
+		]
 	},
 	{
 		title: 'Senior Software Engineer',
@@ -17,8 +26,8 @@ export const workExperience = [
 			'Developed a React app for the brands to interact with the platform.',
 			'Developed a GraphQL API to consume from the different data sources in the platform.',
 			'Automated and orchestrated flows to drive time to costumer down.',
-			'Manage small teams of engineers to plan and implement projects.',
-		],
+			'Manage small teams of engineers to plan and implement projects.'
+		]
 	},
 	{
 		title: 'Software Engineer',
@@ -28,8 +37,8 @@ export const workExperience = [
 			// eslint-disable-next-line quotes
 			"Enabled user's payment of household utilities like gas, electricity, and water.",
 			'Established connection with telecommunication companies to enable users payment of phone and internet services.',
-			'Stored users information that made easy for them to make this actions faster after the first time.',
-		],
+			'Stored users information that made easy for them to make this actions faster after the first time.'
+		]
 	},
 	{
 		title: 'Web Developer',
@@ -38,7 +47,7 @@ export const workExperience = [
 		tasks: [
 			'Upgraded Point-of-Sale UX for a smoother experience while using tablets and phones.',
 			'Migrated web application to a new Bootstrap based template they acquired.',
-			'Enabled application to take orders offline to avoid delaying costumers when wifi wasn’t strong enough. These orders were then synced back with our server as soon as internet connection was recovered.',
-		],
-	},
+			'Enabled application to take orders offline to avoid delaying costumers when wifi wasn’t strong enough. These orders were then synced back with our server as soon as internet connection was recovered.'
+		]
+	}
 ];
